ce5ca6c77cafbc3651c29849a77802af598a5975
Walter Coelho <wcoelho@gmail.com>
2019-06-09 16:27:03 -0300

    Adicionada busca de letras a partir da API do musicmatch

--- src/routes/artist.js
+++ src/routes/artist.js
@@ -2,16 +2,20 @@ const express = require('express')
 
 const router = express()
 
-router.get("/", function (req, res) {    res.send('Artist');
+router.get("/", function (req, res) {    
+    res.send('Artist');
 })
 
-router.post("/", function (req, res) {    res.send('Artist post');
+router.post("/", function (req, res) {    
+    res.send('Artist post');
 })
 
-router.put("/", function (req, res) {    res.send('Artist put');
+router.put("/", function (req, res) {    
+    res.send('Artist put');
 })
 
-router.delete("/", function (req, res) {    res.send('Artist del');
+router.delete("/", function (req, res) {    
+    res.send('Artist del');
 })
   
 module.exports = router;
--- src/routes/lyric.js
+++ src/routes/lyric.js
@@ -1,9 +1,19 @@
 const express = require('express')
+const request = require('request');
 
 const router = express()
 
-router.get("/", function (req, res) {    
-    res.send('Lyric');
+router.get("/", function (req, res) { 
+    const music = req.query["titulo"];
+    const artist = req.query["artista"];//Opcional
+    if(music==null) { res.send("{ \"erro\":\"O título da música é obrigatório\"}"); }
+    
+    var url = 'https://api.musixmatch.com/ws/1.1/matcher.lyrics.get?apikey=c39d3ce1436ee73e524a1774bf47bdc8&q_track='+music;
+    url = (artist!=null)?url+'&q_artist='+artist:url;
+    request(url, { json: true }, (err, res_int, body) => {        
+    if (err) { res.send(err); }
+        res.send(body.message.body.lyrics.lyrics_body);
+    });    
 })
 
 router.post("/", function (req, res) {    
